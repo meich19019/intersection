f1136955829761a4ab44132d7bb3788a9a4c7156
梅智皓 <meich19019@gmail.com>
2022-05-27 15:23:17 +0800

    Initial commit

---
+++ intersection/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace intersection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int intersection in Intersection(new int[] { 1, 5, 9, 6, 5, 2 }, new int[] { 5, 9, 8, 2, 10, 9 }))
+            {
+                Console.WriteLine(intersection);
+            }
+        }
+
+        public static int[] Intersection(int[] a, int[] b)
+        {
+            int[] result;
+            List<int> aList = a.ToList();
+            List<int> bList = b.ToList();
+            result = aList.Intersect(bList).ToArray();
+
+            return result;
+        }
+    }
+}
